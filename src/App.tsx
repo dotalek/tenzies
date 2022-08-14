118467cbb9aadc934e4d7bbdead660ed05c2fe81
Alejandro Duarte <alekduar@gmail.com>
2022-08-13 19:11:53 -0500

    Improved comments in App component

--- src/App.tsx
+++ src/App.tsx
@@ -25,14 +25,17 @@ function App() {
     color: "yellow",
   });
 
+  // This effect handles saving the best time after a game is completed.
+  // It only launches on first render and whenever we achieve tenzies.
   useEffect(() => {
-    console.log("highscore effect");
-    // Save the highest score
-    const noRecord = bestTime.stat === 0;
-    const validTime = currentTime.stat > 0;
-    const newRecord = currentTime.stat < bestTime.stat;
+    const noRecord = bestTime.stat === 0; // No previous record in storage.
+    const validTime = currentTime.stat > 0; // The player has taken at least a second to play.
+    const newRecord = currentTime.stat < bestTime.stat; // The current time is better than the stored one.
+    // Check first if we have a valid record, 0 seconds is impossible to achieve
     if (tenzies && (noRecord || (validTime && newRecord))) {
+      // Update the value in storage.
       localStorage.setItem("bestTime", `${currentTime.stat}`);
+      // Update the value in state.
       setBestTime({
         ...bestTime,
         stat: currentTime.stat,
@@ -40,9 +43,11 @@ function App() {
     }
   }, [tenzies]);
 
+  // This effect handles the timer.
+  // It sets a time out that increases our current time every second.
+  // It only launches while we haven't achieved tenzies and the timer changes.
   useEffect(() => {
     if (!tenzies) {
-      // Increase the timer every second
       setTimeout(
         () =>
           setCurrentTime((prevTime) => ({
@@ -54,31 +59,41 @@ function App() {
     }
   }, [currentTime]);
 
+  // This effect checks for our winning condition; all dice must be held on the same value.
+  // It updates tenzies to true as soon as all dice meet the requirement.
+  // It launches every time our dice change; when we reroll, freeze or start a new game.
   useEffect(() => {
-    // Check everything is held
-    const allHeld = dice.every(({ isHeld }) => isHeld);
-    const firstValue = dice[0].value;
-    // Check for same value
-    const allSame = dice.every(({ value }) => value === firstValue);
+    const allHeld = dice.every(({ isHeld }) => isHeld); // All dice must be frozen/held.
+    const firstValue = dice[0].value; // Use the first dice value as our condition.
+    const allSame = dice.every(({ value }) => value === firstValue); // All dice are the same.
     if (allHeld && allSame) {
       setTenzies(true);
     }
   }, [dice]);
 
+  // Resets our game to the initial state.
+  // Generates new dice, resets our rolls, timer and tenzies status.
   function startOver() {
+    // Generate new dice.
     setDice(allNewDice());
+    // Reset our tenzies status.
     setTenzies(false);
+    // Reset our total rolls.
     setTotalRolls((prevTotalRolls) => ({
       ...prevTotalRolls,
       stat: 0,
     }));
+    // Reset our timer.
     setCurrentTime((prevCurrentTime) => ({
       ...prevCurrentTime,
       stat: 0,
     }));
   }
 
+  // Changes the value of any of our die that aren't considered held.
+  // Every roll counts towards our rolls counter.
   function rollUnheld() {
+    // Reroll unheld dice
     setDice((prevDice) =>
       prevDice.map((die) => {
         if (!die.isHeld) {
@@ -87,12 +102,14 @@ function App() {
         return die;
       }),
     );
+    // Increase our roll count.
     setTotalRolls((prevTotalRolls) => ({
       ...prevTotalRolls,
       stat: prevTotalRolls.stat + 1,
     }));
   }
 
+  // Generate a new set of dice with new values; from 1 - 6, inclusive.
   function allNewDice() {
     const newDice: TDie[] = [];
     for (let i = 1; i <= 10; i++) {
@@ -108,6 +125,7 @@ function App() {
     return newDice;
   }
 
+  // Marks a given dice as held, based on its id.
   function holdDice(id: string) {
     setDice((prevDice) => {
       return prevDice.map((die) => {
@@ -119,6 +137,7 @@ function App() {
     });
   }
 
+  // Build the JSX containing the set of dice.
   const dieElements = dice.map((die) => <Die {...die} />);
 
   return (
